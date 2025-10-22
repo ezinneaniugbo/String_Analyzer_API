cfc8dbe8d1406429f3578cd4b2b8b87d3aef42fc
Queen-Esther <ezinneaniugbo@gmail.com>
2025-10-22 23:39:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -163,5 +163,4 @@ def filter_by_NL():
 
 #To run the app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
